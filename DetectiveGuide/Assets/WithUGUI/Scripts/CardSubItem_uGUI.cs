3f743ca454b6ab751a929454c29c537e64748b10
karion22 <kadas22@naver.com>
2024-07-31 12:13:30 +0900

    중간 작업 커밋

--- DetectiveGuide/Assets/WithUGUI/Scripts/CardSubItem_uGUI.cs
+++ DetectiveGuide/Assets/WithUGUI/Scripts/CardSubItem_uGUI.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CardSubItem_uGUI : MonoBehaviour
 {
-    [SerializeField] private Text m_Title = null;
-    [SerializeField] private Text m_Description = null;
+    [SerializeField] private TMP_Text m_Title = null;
+    [SerializeField] private TMP_Text m_Description = null;
 
     public void SetTitle(string inText) { m_Title.text = inText; }
     public void SetDescription(string inText) {m_Description.text = inText; }
