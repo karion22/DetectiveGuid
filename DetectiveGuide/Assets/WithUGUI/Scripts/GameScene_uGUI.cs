c14ec4c231f56ba31d7dc0f6081ff06d49573610
karion22 <kadas22@naver.com>
2024-08-03 17:59:59 +0900

    기본 작업 중

--- DetectiveGuide/Assets/WithUGUI/Scripts/GameScene_uGUI.cs
+++ DetectiveGuide/Assets/WithUGUI/Scripts/GameScene_uGUI.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using KRN.Utility;
 using TMPro;
+using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,5 +20,28 @@ public class GameScene_uGUI : GameScene
     [SerializeField] private Button m_LogBtn = null;
     #endregion
 
+    [SerializeField] private GameObject m_ItemPrefab = null;
 
+    [ExecuteInEditMode]
+    [ContextMenu("Auto Find UI")]
+    public void AutoFindUI()
+    {
+        m_PlayerCountText = transform.FindEx<TMP_Text>("PlayerCount");
+        m_PlayTurnText = transform.FindEx<TMP_Text>("PlayerTurn");
+        m_SettingBtn = transform.FindEx<Button>("SettingBtn");
+
+        m_ScrollRect = transform.FindEx<ScrollRect>("ScrollView");
+
+        m_LogBtn = transform.FindEx<Button>("LogButton");
+    }
+
+    public override void OnInitialize()
+    {
+        base.OnInitialize();
+
+        for(int i = 0; i < GameMgr.Instance.UserCount; i++)
+        {
+            Instantiate(m_ItemPrefab, m_ScrollRect.content);              
+        }
+    }
 }
